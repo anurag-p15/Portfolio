2165d6da79fd7ba61643852ce278c769d1d0d81d
Anurag Pandey <108535343+anurag-p15@users.noreply.github.com>
2023-11-25 12:08:12 +0530

    Fixed Styling #3

--- portfolio/src/home.js
+++ portfolio/src/home.js
@@ -16,7 +16,7 @@ function Home(){
     <div className="row">
       <div className='logo'>
             <div className="image1">
-            <img src="https://media.licdn.com/dms/image/C4D03AQHWaMz8xN5LQg/profile-displayphoto-shrink_800_800/0/1662729843355?e=1706140800&v=beta&t=PANqjm3FcJ9d8DLXuXYw5GN8Ioxun8oYGfMqEQMIiF0" alt="Sample Image" style={{ position: 'absolute', top: '80', right: '15%', width: '200px', height: '200px' }}/>
+            <img src="https://media.licdn.com/dms/image/C4D03AQHWaMz8xN5LQg/profile-displayphoto-shrink_800_800/0/1662729843355?e=1706140800&v=beta&t=PANqjm3FcJ9d8DLXuXYw5GN8Ioxun8oYGfMqEQMIiF0" alt="Sample Image" style={{ position: 'absolute', top: '80', right: '25%', width: '200px', height: '200px' }}/>
             </div>
             </div>
     </div>
@@ -29,7 +29,7 @@ function Home(){
     </div>
 
     <div className="row">
-      <div className="col-md-6" style={{ marginTop: '2%', marginBottom:'2%' }}>
+      <div className="col-md-6" style={{ marginBottom:'2%' }}>
         <p className="ribbon">
           <span className="text">About Myself</span>
         </p>
@@ -37,19 +37,19 @@ function Home(){
     </div>
     
     <div className='row'>
-        <div className='text' style={{marginTop:'2%'}}>
-            <p style={{textAlign:'left',marginTop:'2%',marginBottom:'2%'}}>Computer Engineering Student from K.J. Somaiya College of Engineering. Interest on problem solving and analysing data and website development.
+        <div className='text'>
+            <p style={{textAlign:'left',marginBottom:'2%'}}>Computer Engineering Student from K.J. Somaiya College of Engineering. Interest on problem solving and analysing data and website development.
                  Also passionate about field of finance and market along with management. An ardent sports lover, represented my school in various competitions and played Inter-District Championships in Basketball.</p>
         </div>
     </div>
 
     <div className="row">
-      <div className="col-md-6" style={{ marginTop: '2%', marginBottom:'2%' }}>
+      <div className="col-md-6" style={{ marginBottom:'2%' }}>
         <p className="ribbon">
           <span className="text"><a href="https://github.com/anurag-p15" style={{textDecoration:'none',color:'black'}}>Click here to see my Github Profile</a></span>
         </p>
       </div>
-      <div className="col-md-6" style={{ marginTop: '2%', marginBottom:'2%' }} >
+      <div className="col-md-6" style={{ marginBottom:'2%' }} >
         <p className="ribbon">
           <span className="text"><a href="https://www.linkedin.com/in/anurag-pandey-10106b241/" style={{textDecoration:'none',color:'black'}}>Click here to see my LinkedIn Profile</a></span>
         </p>
